20f14750729524926d201365fca2c59926a557fe
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-01 12:02:23 -0300

    fix:changing prefix url

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["JSON_SORT_KEYS"] = False
+    app.run(port=5001)
 
     database.init_app(app)
     migration.init_app(app)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, Flask
 from app.routes.lead_route import bp as bp_lead
 
-bp_api = Blueprint("api", __name__, url_prefix="/api")
+bp_api = Blueprint("api", __name__)
 
 def init_app(app: Flask):
     bp_api.register_blueprint(bp_lead)
