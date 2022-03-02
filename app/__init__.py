c2d8e9ff40f2e985a64149e1a35d8ee4454277fd
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-01 22:06:42 -0300

    adding Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ def create_app():
     app = Flask(__name__)
 
     app.config["JSON_SORT_KEYS"] = False
-    app.run(port=5001)
-
     database.init_app(app)
     migration.init_app(app)
     routes.init_app(app)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, Flask
+from flask import Flask
 from app.routes.lead_route import bp as bp_lead
 
-bp_api = Blueprint("api", __name__)
 
 def init_app(app: Flask):
-    bp_api.register_blueprint(bp_lead)
 
-    app.register_blueprint(bp_api)
+    app.register_blueprint(bp_lead)
